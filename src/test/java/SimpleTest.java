c10583a573ed44d204771f4ffcc6584122204e7e
Maruf_Monem <marufmonem@gmail.com>
2022-11-19 11:49:44 +0600

    Created a new test class to access the method

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SimpleTest {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> dataVelue = dataDriven.getData("Purchase");
+
+    }
+}
